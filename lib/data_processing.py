40f060b258e2279fada15455825b05988e989b24
Marshal <wayansmarshalls18@gmail.com>
2026-02-20 18:11:12 -0500

    This is a data-structure-lab

--- lib/data_processing.py
+++ lib/data_processing.py
@@ -9,6 +9,8 @@ def format_student_data(student):
     - Major
     such as: "ID: 10 | Name: Louis Medina | Major: Computer Science"
     """
+    student_id, name, major = student
+    return f"ID: {student_id} | Name: {name} | Major: {major}" 
     pass
 
 def display_students(student_list):
@@ -16,4 +18,6 @@ def display_students(student_list):
     Display all student records.
     Loop through the student_list and print each student using format_student_data().
     """
+    for student in student_list:
+        print(format_student_data(student))
     pass
